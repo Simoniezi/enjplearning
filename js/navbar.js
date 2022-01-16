6f3e7201a84a5a4b93c67dca760210d2798920f9
Simoniezi <thomhav2003@gmail.com>
2022-01-16 23:51:24 +0100

    Navigation bar

---
+++ js/navbar.js
@@ -0,0 +1,12 @@
+function mainDropdown() {
+    document.getElementById("mainDropdown").classList.toggle("show")
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches(".dropbtn")) {
+        var mainDropdown = document.getElementById("mainDropdown");
+        if (mainDropdown.classList.contains("show")) {
+            mainDropdown.classList.remove("show");
+        }
+    }
+}
