a5dbf76e8ed64abe70084d78dd01d1995b22b0dc
Simoniezi <73497522+Simoniezi@users.noreply.github.com>
2022-01-17 02:05:21 +0100

    Update changeLang.js
    changed 言葉 to 言語

--- js/changeLang.js
+++ js/changeLang.js
@@ -13,7 +13,7 @@ function toggleLanguage(language) {
     }
     else if (language === "Japanese") {
         test.innerHTML = "これはホームだ";
-        langOpt.innerHTML = "言葉" + "<i class='fa fa-caret-down' style='padding-left: 4px;'></i>";
+        langOpt.innerHTML = "言語" + "<i class='fa fa-caret-down' style='padding-left: 4px;'></i>";
         en.innerHTML = "英語";
         da.innerHTML = "デンマーク語";
         jp.innerHTML = "日本語";
