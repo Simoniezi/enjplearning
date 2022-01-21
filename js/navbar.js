464da85c3620f44c417184ec1f17db34b1aea2fe
Simoniezi <thomhav2003@gmail.com>
2022-01-21 17:21:35 +0100

    language changing

--- js/navbar.js
+++ js/navbar.js
@@ -12,3 +12,18 @@ window.onclick = function(event) {
         }
     }
 }
+
+// Language menu
+
+function langDropdown() {
+    document.getElementById("langDropdwn").classList.toggle("show")
+}
+
+window.onclick = function langDropdown(event) {
+    if (!event.target.matches(".dropbtn")) {
+        var langDropdown = document.getElementById("langDropdwn");
+        if (langDropdown.classList.contains("show")) {
+            langDropdown.classList.remove("show");
+        }
+    }
+}
