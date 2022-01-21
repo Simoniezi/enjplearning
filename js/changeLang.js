ba84751f403d231ad5dce07787cbd189da960eb7
Simoniezi <thomhav2003@gmail.com>
2022-01-21 19:34:33 +0100

    more language and pages

--- js/changeLang.js
+++ js/changeLang.js
@@ -1,18 +1,25 @@
 // Change between the languages script
 
 function toggleLanguage(language) {
-    let test = document.getElementById("test");
-    let langOpt = document.getElementById("langOpt");
-    let en = document.getElementById("en");
-    let da = document.getElementById("da");
-    let jp = document.getElementById("jp");
     if (language === "Danish") {
-        window.location.replace("indexda.html");
-    }
-    else if (language === "Japanese") {
-        window.location.replace("indexjp.html");
-    }
-    else {
-        window.location.replace("index.html");
-    }
-}
+        if (window.location === "index.html" || "indexjp.html") {
+            window.location.replace("indexda.html");
+        } else if (window.location === "../pages/about.html" || "../pages/aboutjp.html") {
+            window.location.replace("../pages/aboutda.html");
+        } else {
+            return;
+        };
+    } else if (language === "Japanese") {
+        if (window.location === "index.html" || "indexda.html") {
+            window.location.replace("indexjp.html");
+        } else {
+            return;
+        };
+    } else {
+        if (window.location === "indexda.html" || "indexjp.html") {
+            window.location.replace("index.html");
+        } else if (window.location === "../pages/aboutda.html" || "../pages/aboutjp.html") {
+            window.location.replace("../pages/about.html");
+        };
+    };
+};
